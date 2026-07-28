60a03561071e7ae8996db0114ee0215908f05dc6
Bushra1903 <165054883+Bushra1903@users.noreply.github.com>
2026-07-28 19:53:58 +0530

    Update default PORT to 8080

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 const DB_FILE = path.join(__dirname, 'db.json');
 
 app.use(cors());
